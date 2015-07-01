855b8dc1cca7ac9a83a6fbcd89f8450e82b6f4a2
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2015-07-02 00:36:40 +0530

    Adding catch functionality in promise

--- when.js
+++ when.js
@@ -8,7 +8,7 @@
 		var th = this,
 			this.listeners = [],
 			this.catches = [],
-			this.done = false;
+			this.done = this.errored = false;
 
 		cb(function(err, res){
 			th.err = err;
@@ -21,22 +21,37 @@
 
 	when.prototype = {
 		then: function(fn){
-			if(typeof fn == 'function')
-				this.listeners.push(fn);
+			try {
+				if(typeof fn == 'function')
+					this.listeners.push(fn);
 
-			if(this.done) {
-				this.listeners.forEach(function(v){
-					v(this.err, this.res);
-				});
-				this.listeners = [];
+				if(this.done) {
+					this.listeners.forEach(function(v){
+						v(this.err, this.res);
+					});
+					this.listeners = [];
+				}
+			} catch (e) {
+				this.err = e;
+				this.errored = true;
+				this.catch();
 			}
 
 			return this;
 		},
-		// catch: function(fn){
-		// 	this.catches.push(fn);
-		// 	this.done && this.err 
-		// }
+		catch: function(fn){
+			if(typeof fn == 'function')
+				this.listeners.push(fn);
+
+			if (this.errored) {
+				this.catches.forEach(function(v){
+					v(this.err);
+				});
+				this.catches = [];
+			}
+
+			return this;
+		}
 	};
 
 })();
